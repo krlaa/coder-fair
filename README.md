4b8384cbd7c17098a0f3ae515278c011a2281b0f
krlaa <kevindurantony@gmail.com>
2021-11-15 22:18:42 -0500

    first commit & inital mockups

---
+++ README.md
@@ -0,0 +1,2 @@
+# coder_fair
+![Initial design](./design_assets/sample_screen1.png)
---
+++ android/app/src/main/java/com/tcs/coder_fair/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tcs.coder_fair;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
